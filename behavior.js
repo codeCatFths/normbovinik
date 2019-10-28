ac7bdd01b720e5e69ad5fb3a6799564e47cd5e73
codeCatFths <54652836+codeCatFths@users.noreply.github.com>
2019-10-28 10:08:21 -0700

    Add files via upload

---
+++ behavior.js
@@ -0,0 +1,34 @@
+function getInfoOne()
+{
+    document.getElementById( "worldCaption" ).innerHTML = "Norm is walking on the beach in Ventura, checking out this odd remnant of a long ago war at low tide.  Norm is not used to kicking off his shoes at the beach because of the chill so he has left boot prints in the sand. It is late afternoon so he and his find are casting shadows towards the east as the sun lowers in the west.";
+    
+    document.getElementById( "worldImage" ).src = "images/carrc-p02p03a.jpg";
+} // end getInfoOne()
+
+function getInfoTwo()
+{
+    document.getElementById( "worldCaption" ).innerHTML = "In his secret cybersecurity and coding work for the government, Norm travels a lot to investigate conspiracy theories. Here he is checking out some rumors about the Apollo 11 moon lander during a work trip to Seattle. In reflecting on the history of this artifact, he ponders history and tries to see himself in the greater scheme of things.";
+    
+    document.getElementById( "worldImage" ).src = "images/carrc-p02p03b.jpg";
+} // end getInfoTwo()
+
+function getInfoThree()
+{
+    document.getElementById( "worldCaption" ).innerHTML = "Norm is visiting his friends from high school up in northern California for his 10 year high school reunion and taking a quick day trip to MacKerricher State Park near Fort Bragg.";
+    
+    document.getElementById( "worldImage" ).src = "images/carrc-p02p03c.jpg";
+} // end getInfoThree()
+
+function getInfoFour()
+{
+    document.getElementById( "worldCaption" ).innerHTML = "Norm needed fresh herbs to plant in the kitchen garden so he stopped at Green Thumb Nursery. His life is a series of unexpected events...and so...he ran into a velociraptor rampaging at the garden store today. He remembered something from that movie...something about standing still so the dinosaur can't see you???!!!";
+    
+    document.getElementById( "worldImage" ).src = "images/carrc-p02p03d.jpg";
+} // end getInfoFour()
+
+function getInfoFive()
+{
+    document.getElementById( "worldCaption" ).innerHTML = "When Norm is searching for tranquility in life, he heads up to the Tar Pitz at Carpinteria State Park.  It brings back happy memories of traveling from northern California to the warmer beaches in summer for some camping with his parents and his sister, Hecate. One can stand on the shore watching the surfers, the dolphins swimming by, and the pelicans soaring past a wingstip-span above the breaking waves.";
+    
+    document.getElementById( "worldImage" ).src = "images/carrc-p02p03e.jpg";
+} // end getInfoFive()
