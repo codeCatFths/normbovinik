617658b3171bc5e54ccd10ea2fa27d709ddc79bc
codeCatFths <54652836+codeCatFths@users.noreply.github.com>
2019-12-10 18:11:07 -0800

    Add files via upload

---
+++ scenes.js
@@ -0,0 +1,36 @@
+function getScene01()
+{
+    document.getElementById( "sceneCaption" ).innerHTML = "Norm is traveling for business to Seattle. While on his business trip, he is planning some recreational time to visit the Museum of Flight to see the Apollo 11 exhibit because he is a sci-fi geek. He wants to inject some fun into his visit.";
+    
+    document.getElementById( "sceneImage" ).src = "media/scene01.png";
+} // end getInfoOne()
+
+function getScene02()
+{
+    document.getElementById( "sceneCaption" ).innerHTML = "Traveling from home causes Norm anxiety. Staying in hotels causes Norm stress because he is never quite comfortable. He never rests easy when way from home. This causes him to have stress dreams. His dreamscape includes aliens and flying saucers due to the combination of watching Signs before bed, having the Space Needle visible from his room and the anticipation of seeing the moon lander from Apollo 11 - a subconscious theme develops in terms of little green men from the moon.";
+    
+    document.getElementById( "sceneImage" ).src = "media/scene02.png";
+} // end getInfoTwo()
+
+function getScene03()
+{
+    document.getElementById( "sceneCaption" ).innerHTML = "On Norm's trip to the Apollo 11 exhibit, he is suffering from lack of sleep the night before and stress from the travel. This combination is causing him to see significance in coincidences and to feel like he is seeing things from the corners of his eyes. The alien and flying saucer theme continues from his stress dreams the night before.";
+    
+    document.getElementById( "sceneImage" ).src = "media/scene03.png";
+} // end getInfoThree()
+
+function getScene04()
+{
+    document.getElementById( "sceneCaption" ).innerHTML = "As Norm is visiting the moon lander portion of the Apollo exhibit, he actually believes he is seeing aliens inside the moon lander. This freaks him out.";
+    
+    document.getElementById( "sceneImage" ).src = "media/scene04.png";
+} // end getInfoFour()
+
+function getScene05()
+{
+    document.getElementById( "sceneCaption" ).innerHTML = "An old college roommate, planning to surprise Norm at the museum, decides to play a prank and brings an alien mask to the exhibit. From their past interactions, his friend knows about Norm's keen interest in sci-fi and his uneasiness during travel. This friend was trying to coax Norm out of his overly reticent mood and get him to lighten up and have fun. Moral-Message: Don't let stress get to you and ruin the fun times.";
+    
+    document.getElementById( "sceneImage" ).src = "media/scene05.png";
+} // end getInfoFive()
+
+
